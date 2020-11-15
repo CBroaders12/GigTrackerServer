0c9f617b9abfa3cb88e578f837bf7b67f593452b
CBroaders <67294112+CBroaders12@users.noreply.github.com>
2020-11-15 11:29:11 -0500

    admin user seeder file

---
+++ database/seeders/20201115162233-admin-user.js
@@ -0,0 +1,17 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('users', [{
+      email: 'admin@email.com',
+      password: bcrypt.hashSync('adminpassword', 10),
+      userType: 'admin',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }]);
+  },
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('users', null, {});
+  }
+};
